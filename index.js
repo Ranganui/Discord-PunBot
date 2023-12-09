77c5d1e0c8eec09fd90355d2b2c8bc61d2efd4be
Ranganui <robert.bishop.tester@gmail.com>
2023-12-10 11:56:05 +1300

    Init
    As Title

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
