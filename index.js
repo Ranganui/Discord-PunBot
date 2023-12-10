2b7bd00cea6fed08f127f02a0d464f36cc5fd769
Ranganui <robert.bishop.tester@gmail.com>
2023-12-10 13:03:47 +1300

    KeyPair
    As Title

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
+const puns = {
+  cat: 'I’m feline good!',
+  fish: 'You’ve got to be squidding me!',
+  ghost: 'That’s a little bit spooky!',
+};
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', (message) => {
+  console.log('Message content:', message.content); // Log only the content
   if (message.author.bot) return;
-  if (message.content === 'ping') {
-    message.reply('pong');
+
+  const messageContent = message.content.toLowerCase();
+  const foundKey = Object.keys(puns).find((key) =>
+    messageContent.includes(key)
+  );
+
+  if (foundKey) {
+    message
+      .reply(puns[foundKey])
+      .catch((error) => console.error('Error sending reply:', error));
   }
 });
 
